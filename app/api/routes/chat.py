d38c2ce561e8d2e3e076bf932d8952eb6df71718
Reneesh28 <reneesh3508925@gmail.com>
2026-04-12 18:42:07 +0530

    Updating

--- app/api/routes/chat.py
+++ app/api/routes/chat.py
@@ -1,11 +1,15 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Header
 from app.schemas.chat_schema import ChatRequest, ChatResponse
 from app.services.llm_service import generate_response
+from app.core.config import INTERNAL_API_KEY
 
 router = APIRouter()
 
 @router.post("/")
-def chat_endpoint(request: ChatRequest):
+def chat_endpoint(request: ChatRequest, x_api_key: str = Header(None)):
+    if x_api_key != INTERNAL_API_KEY:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+
     try:
         ai_response = generate_response(request.message)
         return ChatResponse(response=ai_response)
